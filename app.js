48f8c96f24f523795b6eeefc7cfdc0aa10e16de9
Marv_Tron <marvinatryonjr@gmail.com>
2019-04-19 13:31:17 -0700

    app

--- app.js
+++ app.js
@@ -7,11 +7,31 @@ const path = require('path');
 db = require('./config/database');
 
 // Test Database
-db.authenticate()
+ db.authenticate()
   .then(() => console.log('Database connected...'))
   .catch(err => console.log('Error: ' + err));
 
 const app = express();
 
 // Handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Index Route
+app.get('/', (req, res) => res.render('index', { layout: 'landing' }));
+
+// Gigs Route
+app.use('/gigs', require('./routes/gigs'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port https://localhost://${PORT}`));
+
+
 
